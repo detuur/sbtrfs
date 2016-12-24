2a7deb808f7ab713f120de3e1ae2cf34236da17f
detuur <detuur@gmail.com>
2016-12-24 22:18:48 +0100

    Reorganised repo; started implementing FS

--- sbtrfs/src/main.rs
+++ sbtrfs/src/main.rs
@@ -1,3 +1,29 @@
+extern crate fuse;
+extern crate libc;
+extern crate time;
+
+use std::path::Path;
+use libc::{ENOENT, ENOSYS};
+use time::Timespec;
+use std::env;
+use fuse::{FileAttr, FileType, Filesystem, Request, ReplyAttr, ReplyData, ReplyEntry, ReplyDirectory};
+
+struct SbFilesystem;
+
+impl Filesystem for SbFilesystem {
+    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+        println!("getattr(ino={})", ino);
+        reply.error(ENOSYS);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mountpoint = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("Usage: {} <MOUNTPOINT>", env::args().nth(0).unwrap());
+            return;
+        }
+    };
+    fuse::mount(SbFilesystem, &mountpoint, &[]);
 }
